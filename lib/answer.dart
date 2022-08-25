b36c2abe451aa40d190abd2ad61ef7e0c2565ffb
ayush8808 <1si20et008@sit.ac.in>
2022-08-25 23:23:57 +0530

    argument is pass

--- lib/answer.dart
+++ lib/answer.dart
@@ -1,19 +1,19 @@
 import 'package:flutter/material.dart';
 
 class Answer extends StatelessWidget {
-  final VoidCallback? handler;
+  final Function handler;
   Answer(this.handler);
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: ElevatedButton(onPressed: () => handler,
-        child: Text('Option 3'),
+      child:ElevatedButton(
+        onPressed:() => handler,
+        child: Text('Option 1'),
         style: ElevatedButton.styleFrom(
-            primary: Colors.purple
+          primary: Colors.purple,
         ),
       ),
     );
   }
 }
-
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import './question.dart';
 import './answer.dart';
+
 void main() => runApp(_MyApp());
 
 class _MyApp extends StatefulWidget {
@@ -10,44 +11,46 @@ class _MyApp extends StatefulWidget {
   }
 }
 
-class _MyAppState extends State <_MyApp>{
-  var _questionIndex = 1;
+class _MyAppState extends State<_MyApp> {
+  var _questionIndex = 0;
   void _AnsQues(a) {
     setState(() {
       _questionIndex += 1;
-       if(_questionIndex == a.length){
-        _questionIndex=0;
+      if (_questionIndex == a.length) {
+        _questionIndex = 0;
       }
-
     });
   }
+
   @override
   Widget build(BuildContext context) {
-    var _question = ['What is your favarite animal ?',
-                    'What is your favorite color ?',];
+    var _question = [
+      'What is your favarite animal ?',
+      'What is your favorite color ?',
+    ];
     return MaterialApp(
-
-      debugShowCheckedModeBanner: false,
+        debugShowCheckedModeBanner: false,
         home: Scaffold(
-            appBar: AppBar(
-              title: Text('First App'),
-            ),
-            body: Center(
-              child: Column(
-                children: [
-                  Question(_question[_questionIndex],
+          appBar: AppBar(
+            title: Text('First App'),
+          ),
+          body: Center(
+            child: Column(
+              children: [
+                Question(
+                  _question[_questionIndex],
+                ),
+                ElevatedButton(
+                  onPressed: () => _AnsQues(_question),
+                  child: Text("Option 1"),
+                  style: ElevatedButton.styleFrom(
+                    primary: Colors.purple,
                   ),
-                  ElevatedButton(onPressed: () => _AnsQues,
-                    child: Text('Option 3'),
-                    style: ElevatedButton.styleFrom(
-                        primary: Colors.purple
-                    ),
-                  ),
-                ],
-              ),
+                )
+              ],
             ),
-            backgroundColor:Colors.cyan[100],
-        )
-    );
+          ),
+          backgroundColor: Colors.cyan[100],
+        ));
   }
 }
